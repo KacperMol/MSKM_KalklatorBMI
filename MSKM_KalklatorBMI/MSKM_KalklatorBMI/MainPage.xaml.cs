63a34fc59336cec690f0d3630fb5193389fa6fef
Miłosz Szubryt <mszubryt2005@gmail.com>
2024-03-05 12:46:37 +0100

    dodaj Zapisz_Clicked

--- MSKM_KalklatorBMI/MSKM_KalklatorBMI/MainPage.xaml.cs
+++ MSKM_KalklatorBMI/MSKM_KalklatorBMI/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace MSKM_KalklatorBMI
@@ -13,6 +15,25 @@ namespace MSKM_KalklatorBMI
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Zapisz_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(imietxt.Text) && !string.IsNullOrWhiteSpace(nazwiskotxt.Text))
+            {
+                await App.Database.ZapiszUzytkownikaAsync(new Uzytkownik
+                {
+                    Imie = imietxt.Text,
+                    Nazwisko = nazwiskotxt.Text,
+                    BMI = double.Parse(BMI.Text),
+                });
+                imietxt.Text = nazwiskotxt.Text = string.Empty;
+                await DisplayAlert("Sukces", "Dodano dane do bazy", "OK");
+
+            }
+            else
+                await DisplayAlert("Blad", "Wpisz imie i nazwisko", "OK");
+        
         }
     }
 }
