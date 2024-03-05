ddeaa52437b0ff2822f267ce1ee210435c00f3b4
Miłosz Szubryt <mszubryt2005@gmail.com>
2024-03-05 12:03:26 +0100

    dodaj klase uzytkownik

---
+++ MSKM_KalklatorBMI/MSKM_KalklatorBMI/Uzytkownik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSKM_KalklatorBMI
+{
+    public  class Uzytkownik
+    {
+        [PrimaryKey,AutoIncrement]
+        public int ID { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public double BMI { get; set; }
+    }
+}
